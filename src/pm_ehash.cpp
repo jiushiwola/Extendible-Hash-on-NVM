08b4fab53a2a22bfe5c1db98a395638f6c6ee433
w-hn <2270654937@qq.com>
2020-06-23 21:53:54 +0800

    增加增删改查函数“
    git commit -m 增加增删改查函数
    
    git commit -m 增加增删改查函数“

--- src/pm_ehash.cpp
+++ src/pm_ehash.cpp
@@ -136,3 +136,165 @@ void PmEHash::writeBack() {
 
 }
 
+
+
+/**
+ * @description:
+ * @param kv: 插入的键值对
+ * @return: 0 = insert successfully, -1 = fail to insert(target data doesn't exist)
+ */
+int PmEHash::insert(kv new_kv_pair) {
+
+    uint64_t temp;
+    if (search(new_kv_pair.key, temp) == 0)
+        return -1;
+
+    pm_bucket* bucket = getFreeBucket(new_kv_pair.key);
+
+    kv* freePlace = getFreeKvSlot(bucket);
+    *freePlace = new_kv_pair;
+    persist(freePlace, sizeof(freePlace));
+    return 0;
+}
+
+/**
+ * @description:
+ * @param uint64_t: 要删除的目标键值对的键
+ * @return: 0 = removing successfully, -1 = fail to remove(target data doesn't exist)
+ */
+int PmEHash::remove(uint64_t key) {
+    //uint64_t temp;
+    //if (search(key, temp) == -1) return -1;
+
+    uint64_t hash_key = hashFunc(key);
+    uint64_t mask = (1 << metadata->global_depth) - 1;
+    uint64_t catalogID = hash_key & mask;
+
+    pm_bucket* bucket = bucketTable[catalogID];
+    for (int i = 0; i < BUCKET_SLOT_NUM / 8; ++i) {
+        uint8_t bitmap = bucket->bitmap[i];
+        for (int j = 0; j < 8; ++j) {
+            if (((bitmap >> (7 - j)) & 1) == 1 && bucket->slot[i*8+j].key == key) {//找到桶中的一个kv对且key相匹配
+                bucket->bitmap[i] &= ~(1 << (7- j)); //将该位置为空闲状态
+                persist(&bucket->bitmap[i], sizeof(bucket->bitmap[i]));
+                //是否合并交给mergeBucket自己决定
+                mergeBucket(catalogID);
+                return 0; //一个key只能对应一个value
+            }
+        }
+    }
+    return -1;
+
+}
+/**
+ * @description: 更新现存的键值对的值
+ * @param kv: 更新的键值对，有原键和新值
+ * @return: 0 = update successfully, -1 = fail to update(target data doesn't exist)
+ */
+int PmEHash::update(kv kv_pair) {
+    //uint64_t temp;
+    //if (search(kv_pair.key, temp) == -1) return -1;
+    pm_bucket* bucket = key_to_bucket(kv_pair.key);
+    for (int i = 0; i < BUCKET_SLOT_NUM / 8; ++i) {
+        uint8_t bitmap = bucket->bitmap[i];
+        for (int j = 0; j < 8; ++j) {
+            if (((bitmap >> (7 - j)) & 1) == 1 && bucket->slot[i*8+j].key == kv_pair.key) {//找到桶中的一个kv对且key相匹配
+                bucket->slot[i*8+j].value = kv_pair.value;
+                persist(&bucket->slot[i*8+j], sizeof(bucket->slot[i*8+j]));
+                return 0; //一个key只能对应一个value
+            }
+        }
+    }
+    return -1;
+}
+/**
+ * @description:
+ * @param uint64_t: 查询的目标键
+ * @param uint64_t&: 查询成功后返回的目标值
+ * @return: 0 = search successfully, -1 = fail to search(target data doesn't exist)
+ */
+int PmEHash::search(uint64_t key, uint64_t& return_val) {
+
+    uint64_t hash_key = hashFunc(key);
+    uint64_t mask = (1 << metadata->global_depth) - 1;
+    uint64_t catalogID = hash_key & mask;
+    pm_bucket* bucket = bucketTable[catalogID];
+    for (int i = 0; i < BUCKET_SLOT_NUM / 8; ++i) {
+        uint8_t bitmap = bucket->bitmap[i];
+        for (int j = 0; j < 8; ++j) {
+            if (((bitmap >> (7 - j)) & 1) == 1 ) {//找到桶中的一个kv对且key相匹配
+                if (bucket->slot[i*8+j].key == key) {
+                    return_val = bucket->slot[i*8+j].value;
+                    return 0;
+                }
+
+            }
+        }
+    }
+    return -1;
+}
+
+/**
+ * @description: 用于对输入的键产生哈希值，用于后续的取模求桶号操作
+ * @param uint64_t: 输入的键
+ * @return: 返回键的哈希值
+ */
+uint64_t PmEHash::hashFunc(uint64_t key) {
+//https://www.cnblogs.com/thrillerz/p/4516769.html
+    key = (~key) + (key << 21); // key = (key << 21) - key - 1;
+    key = key ^ (key >> 24);
+    key = (key + (key << 3)) + (key << 8); // key * 265
+    key = key ^ (key >> 14);
+    key = (key + (key << 2)) + (key << 4); // key * 21
+    key = key ^ (key >> 28);
+    key = key + (key << 31);
+    return key;
+}
+
+/**
+ * @description: 获得供插入的空闲的桶，无空闲桶则先分裂桶然后再返回空闲的桶
+ * @param uint64_t: 带插入的键
+ * @return: 空闲桶的虚拟地址
+ */
+pm_bucket* PmEHash::getFreeBucket(uint64_t key) {
+
+    uint64_t hash_key = hashFunc(key);
+    uint64_t mask = (1 << metadata->global_depth) - 1;
+    uint64_t catalogID = hash_key & mask;
+
+    pm_bucket* bucket = bucketTable[catalogID];
+    for (int i = 0; i < BUCKET_SLOT_NUM / 8; ++i) {
+        uint8_t bitmap = bucket->bitmap[i];
+        for (int j = 0; j < 8; ++j) {
+            if (((bitmap >> (7 - j)) & 1) == 0) {//找到桶中的第一个空位
+                return bucket;
+            }
+        }
+    }
+
+    splitBucket(catalogID);
+    //重新调用自己
+    return getFreeBucket(key);
+
+}
+
+/**
+ * @description: 获得空闲桶内第一个空闲的位置供键值对插入
+ * @param pm_bucket* bucket
+ * @return: 空闲键值对位置的虚拟地址
+ */
+kv* PmEHash::getFreeKvSlot(pm_bucket* bucket) {
+    for (int i = 0; i < BUCKET_SLOT_NUM / 8; ++i) {
+        uint8_t bitmap = bucket->bitmap[i];
+        for (int j = 0; j < 8; ++j) {
+            if (((bitmap >> (7 - j)) & 1) == 0) {
+                bucket->bitmap[i] |= (1 << (7 - j)); //标记为被占用
+                persist(&bucket->bitmap[i], sizeof(bucket->bitmap[i]));
+                return &bucket->slot[i*8+j];
+            }
+        }
+    }
+    return NULL;
+}
+
+

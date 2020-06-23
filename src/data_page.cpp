11c216ce48b0018b10f29cb1fe11f7cbe8266739
xumy <2474965565@qq.com>
2020-06-23 22:18:34 +0800

    增加合并、回收页、打印、makefile

--- src/data_page.cpp
+++ src/data_page.cpp
@@ -3,6 +3,19 @@
 #include <iostream>
 
 extern const char* PM_EHASH_DIRECTORY;        // add your own directory path to store the pm_ehash
+void PmEHash::deletePage(uint32_t page_id) {
+    cout << "Delete page " << page_id << endl;
+    page_pointers[page_id]->in_memory = page_in_memory[page_id] = false;
+    persist(&page_pointers[page_id]->in_memory, sizeof(page_pointers[page_id]->in_memory));
+    pmem_unmap(page_pointers[page_id], sizeof(data_page));
+//更新free_list
+    int cnt = free_list.size();
+    for (int i = 0; i < cnt; ++i) {
+        pm_bucket* bucket = free_list.front();
+        free_list.pop();
+        if (page_in_memory[vAddr2pmAddr[bucket].fileId] == true) free_list.push(bucket);
+    }
+}
 
 void PmEHash::mapAllPage() {
 //将页面作内存映射，映射到pmem0挂载的地方;
@@ -66,3 +79,41 @@ void PmEHash::mapAllPage() {
 
     }
 }
+
+void PmEHash::allocNewPage() {
+    int id = 0;
+    for (uint64_t i = 1; i < metadata->max_file_id; ++i) {
+        if (page_in_memory[i] == false) {
+            id = i;
+            break;
+        }
+    }
+    if (id == 0 && metadata->max_file_id == MAX_PAGE_ID) {
+        printf("failed to allocate new page.\n");
+        return;
+    }
+    if (id == 0) id = metadata->max_file_id++;
+    char filename[30];
+    sprintf(filename, "%s%d", PM_EHASH_DIRECTORY, id);
+    int PMEM_LEN = (sizeof(pm_bucket) + 1) * DATA_PAGE_SLOT_NUM;
+    size_t mapped_len;
+    if ((page_pointers[id] = (data_page*)pmem_map_file(filename, PMEM_LEN, PMEM_FILE_CREATE,
+        0666, &mapped_len, &Is_pmem[id])) == NULL) {
+        perror("pmem_map_file");
+        exit(1);
+    }
+    page_pointers[id]->in_memory = page_in_memory[id] = true;
+
+    for (int i = 0; i < DATA_PAGE_SLOT_NUM; ++i) {
+        pm_address address(id, DATA_PAGE_SLOT_NUM + i * sizeof(pm_bucket));
+        pm_bucket* bucket = &page_pointers[id]->buckets[i];
+        bucket->local_depth = 4;
+        page_pointers[id]->used[i] = false;
+        vAddr2pmAddr[bucket] = address;
+        free_list.push(bucket);
+    }
+    persist(metadata, sizeof(ehash_metadata));
+    persist(page_pointers[id], sizeof(data_page));
+}
+
+
